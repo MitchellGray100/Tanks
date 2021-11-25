a75cd40c6fbe30f5cb82a2d3562fbbc9bf641004
Mitchell Gray <meg346@cornell.edu>
2021-11-25 13:39:17 -0500

    Tank collision:

--- src/application/Main.java
+++ src/application/Main.java
@@ -53,12 +53,12 @@ public class Main extends Application {
 					case BULLET:
 						break;
 					case POWERUP:
-						root.getChildren().add(new Piece(r*100,c * 100,100,100, "powerup", Color.SKYBLUE,r,c));
+						root.getChildren().add(new Piece(r*100+25,c * 100+25,50,50, "powerup", Color.SKYBLUE,r,c));
 						break;
 					case TANK:
 						if(controller.getSquarePiece(r, c).getPlayer() == piece.Piece.Player.ONE)
 						{
-							tankOne = new Piece(r*100,c * 100+12,50,50, "tankOne", Color.GREEN,r,c);
+							tankOne = new Piece(r*100,c * 100+50,50,50, "tankOne", Color.GREEN,r,c);
 							root.getChildren().add(tankOne);
 						}
 						else
@@ -100,70 +100,196 @@ public class Main extends Application {
 					
 					break;
 				case "tankOne":
-					
 						if(tankOneMoveRight)
 						{
+							if(!tankOneMoveUp && !tankOneMoveDown)
+							{
+								((piece.Tank)controller.getSquarePiece(tankOne.r, tankOne.c)).setDirection(piece.Piece.Direction.RIGHT);
+								for(Piece piece: brickList)
+								{
+									if(tankOne.getBoundsInParent().intersects(piece.r * 100, piece.c * 100+5, 10, 90))
+									{
+										s.moveLeft();
+									}
+								}
+								s.moveRight();
+							}
+							if(tankOne.getBoundsInParent().intersects(tankTwo.getTranslateX(), tankTwo.getTranslateY()+5, 10, 40))
+							{
+								s.moveLeft();
+							}
+						}
+						if(tankOneMoveLeft)
+						{
+							if(!tankOneMoveUp && !tankOneMoveDown)
+							{
+								((piece.Tank)controller.getSquarePiece(tankOne.r, tankOne.c)).setDirection(piece.Piece.Direction.LEFT);
+								for(Piece piece: brickList)
+								{
+									if(tankOne.getBoundsInParent().intersects(piece.r * 100+90, piece.c * 100+5, 10, 90))
+									{
+										s.moveRight();
+									}
+								}
+								if(tankOne.getBoundsInParent().intersects(tankTwo.getTranslateX()+40, tankTwo.getTranslateY()+5, 10, 40))
+								{
+									s.moveRight();
+								}
+								s.moveLeft();
+							}
+						}
+						if(tankOneMoveUp)
+						{
+//							if(!tankOneMoveLeft && !tankOneMoveRight)
+							{
+								((piece.Tank)controller.getSquarePiece(tankOne.r, tankOne.c)).setDirection(piece.Piece.Direction.UP);
+								for(Piece piece: brickList)
+								{
+									if(tankOne.getBoundsInParent().intersects(piece.r * 100+5, piece.c * 100+90, 90, 10))
+									{
+										s.moveDown();
+									}
+								}
+								if(tankOne.getBoundsInParent().intersects(tankTwo.getTranslateX()+5, tankTwo.getTranslateY()+40, 40, 10))
+								{
+									s.moveDown();
+								}
+								s.moveUp();
+							}
+						}
+						if(tankOneMoveDown)
+						{
+//							if(!tankOneMoveLeft && !tankOneMoveRight)
+							{
+								((piece.Tank)controller.getSquarePiece(tankOne.r, tankOne.c)).setDirection(piece.Piece.Direction.DOWN);
+								for(Piece piece: brickList)
+								{
+									if(tankOne.getBoundsInParent().intersects(piece.r * 100+5, piece.c * 100, 90, 10))
+									{
+										s.moveUp();
+									}
+								}
+								if(tankOne.getBoundsInParent().intersects(tankTwo.getTranslateX()+5, tankTwo.getTranslateY(), 40, 10))
+								{
+									s.moveUp();
+								}
+								s.moveDown();
+							}
+						}
+						switch(((piece.Tank)controller.getSquarePiece(tankOne.r, tankOne.c)).getDirection())
+						{
+						case DOWN:
+							tankOne.setRotate(180);
+							break;
+						case LEFT:
+							tankOne.setRotate(270);
+							break;
+						case RIGHT:
+							tankOne.setRotate(90);
+							break;
+						case UP:
+							tankOne.setRotate(0);
+							break;
+						default:
+							break;
+						
+						}
+					break;
+				case "tankTwo":
+					if(tankTwoMoveRight)
+					{
+						if(!tankTwoMoveUp && !tankTwoMoveDown)
+						{
+							((piece.Tank)controller.getSquarePiece(tankTwo.r, tankTwo.c)).setDirection(piece.Piece.Direction.RIGHT);
 							for(Piece piece: brickList)
 							{
-								if(tankOne.getBoundsInParent().intersects(piece.r * 100, piece.c * 100+5, 10, 90))
+								if(tankTwo.getBoundsInParent().intersects(piece.r * 100, piece.c * 100+5, 10, 90))
 								{
 									s.moveLeft();
 								}
 							}
+							if(tankTwo.getBoundsInParent().intersects(tankOne.getTranslateX(), tankOne.getTranslateY()+5, 10, 40))
+							{
+								s.moveLeft();
+							}
 							s.moveRight();
 						}
-						if(tankOneMoveLeft)
+					}
+					if(tankTwoMoveLeft)
+					{
+						if(!tankTwoMoveUp && !tankTwoMoveDown)
 						{
+							((piece.Tank)controller.getSquarePiece(tankTwo.r, tankTwo.c)).setDirection(piece.Piece.Direction.LEFT);
 							for(Piece piece: brickList)
 							{
-								if(tankOne.getBoundsInParent().intersects(piece.r * 100+90, piece.c * 100+5, 10, 90))
+								if(tankTwo.getBoundsInParent().intersects(piece.r * 100+90, piece.c * 100+5, 10, 90))
 								{
 									s.moveRight();
 								}
 							}
+							if(tankTwo.getBoundsInParent().intersects(tankOne.getTranslateX()+40, tankOne.getTranslateY()+5, 10, 40))
+							{
+								s.moveRight();
+							}
 							s.moveLeft();
 						}
-						if(tankOneMoveUp)
+					}
+					if(tankTwoMoveUp)
+					{
+//						if(!tankTwoMoveLeft && !tankTwoMoveRight)
 						{
+							((piece.Tank)controller.getSquarePiece(tankTwo.r, tankTwo.c)).setDirection(piece.Piece.Direction.UP);
 							for(Piece piece: brickList)
 							{
-								if(tankOne.getBoundsInParent().intersects(piece.r * 100+5, piece.c * 100+90, 90, 10))
+								if(tankTwo.getBoundsInParent().intersects(piece.r * 100+5, piece.c * 100+90, 90, 10))
 								{
 									s.moveDown();
 								}
 							}
+							if(tankTwo.getBoundsInParent().intersects(tankOne.getTranslateX()+5, tankOne.getTranslateY()+40, 40, 10))
+							{
+								s.moveDown();
+							}
 							s.moveUp();
 						}
-						if(tankOneMoveDown)
+					}
+					if(tankTwoMoveDown)
+					{
+//						if(!tankTwoMoveLeft && !tankTwoMoveRight)
 						{
+							((piece.Tank)controller.getSquarePiece(tankTwo.r, tankTwo.c)).setDirection(piece.Piece.Direction.DOWN);
 							for(Piece piece: brickList)
 							{
-								if(tankOne.getBoundsInParent().intersects(piece.r * 100+5, piece.c * 100, 90, 10))
+								if(tankTwo.getBoundsInParent().intersects(piece.r * 100+5, piece.c * 100, 90, 10))
 								{
 									s.moveUp();
 								}
 							}
+							if(tankTwo.getBoundsInParent().intersects(tankOne.getTranslateX()+5, tankOne.getTranslateY(), 40, 10))
+							{
+								s.moveUp();
+							}
 							s.moveDown();
 						}
-					break;
-				case "tankTwo":
-						if(tankTwoMoveRight)
-						{
-							s.moveRight();
-						}
-						if(tankTwoMoveLeft)
-						{
-							s.moveLeft();
-						}
-						if(tankTwoMoveUp)
-						{
-							s.moveUp();
-						}
-						if(tankTwoMoveDown)
-						{
-							s.moveDown();
-						}
+					}
+					switch(((piece.Tank)controller.getSquarePiece(tankTwo.r, tankTwo.c)).getDirection())
+					{
+					case DOWN:
+						tankTwo.setRotate(180);
+						break;
+					case LEFT:
+						tankTwo.setRotate(270);
+						break;
+					case RIGHT:
+						tankTwo.setRotate(90);
+						break;
+					case UP:
+						tankTwo.setRotate(0);
+						break;
+					default:
+						break;
 					
+					}
 					break;
 					
 				}
@@ -184,12 +310,19 @@ public class Main extends Application {
 		boolean dead = false;
 		final String type;
 		Rectangle border;
+		Rectangle indication;
 		int r;
 		int c;
 		Piece(int x, int y, int w, int h, String type, Color color, int r, int c)
 		{
+			indication = new Rectangle(10,5,Color.BLACK);
+			indication.setTranslateY(this.getTranslateY()-25);
 			border = new Rectangle(w,h,color);
 			getChildren().addAll(border);
+			if(type.equals("tankOne") || type.equals("tankTwo"))
+			{
+				getChildren().add(indication);
+			}
 			this.type = type;
 			setTranslateX(x);
 			setTranslateY(y);
