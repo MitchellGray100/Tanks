7725b181309ba516049254f0d73f9d89c8793298
Mitchell <r2d2mg200@gmail.com>
2022-01-12 15:36:55 -0500

    Fixed AI not moving

--- src/application/Main.java
+++ src/application/Main.java
@@ -384,6 +384,7 @@ public class Main extends Application {
 					}
 					break;
 				case "tankOne":
+
 					tankOneMoveLeft = false;
 					tankOneMoveRight = false;
 					tankOneMoveUp = false;
@@ -499,11 +500,12 @@ public class Main extends Application {
 					}
 					break;
 				case "tankTwo":
-
-					tankTwoMoveLeft = false;
-					tankTwoMoveRight = false;
-					tankTwoMoveUp = false;
-					tankTwoMoveDown = false;
+					if (twoPlayers) {
+						tankTwoMoveLeft = false;
+						tankTwoMoveRight = false;
+						tankTwoMoveUp = false;
+						tankTwoMoveDown = false;
+					}
 					if (playerTwoControls.size() > 0) {
 						for (Character chars : playerTwoControls) {
 							System.out.print((Character) chars);
